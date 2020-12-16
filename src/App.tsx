c3ccf3e2291292906e2f7e1c0b4e8be97af5310e
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2020-12-16 11:32:19 +0100

    Clean up av imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import Panel from "nav-frontend-paneler";
-import Navn from "./components/navn";
-import Status from "./components/status";
-import OkonomiRadDagpenger from "./components/okonomi/okonomi-rad-dagpenger";
 import OkonomiRad from "./components/okonomi/okonomi-rad";
 
 function App() {
