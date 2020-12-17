cb6a26b85b95e12d2e61fbc737b2355b0ec924eb
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2020-12-17 12:58:33 +0100

    Flytter henting av data ut til App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,48 @@
 import React from "react";
 import "./App.css";
 import OkonomiRad from "./components/okonomi/okonomi-rad";
+import useSWR from "swr";
+import { authUrl, brukerinfoUrl, oppfolgingUrl, underOppfolgingUrl } from "./url";
+
+interface Authdata {
+  authenticated: boolean;
+  securityLevel: "3" | "4";
+  name: string;
+}
+
+interface Oppfolgingdata {
+  underOppfolging: boolean;
+}
+
+interface Brukerdata {
+  erSykmeldtMedArbeidsgiver: boolean;
+}
+
+const fetcher = async (url: string) => {
+  const response = await fetch(url, { method: "GET", credentials: "include" });
+  const data = await response.json();
+  return data;
+};
 
 function App() {
+  const { data: auth } = useSWR<Authdata>(authUrl, fetcher);
+
+  const { data: underOppfolgingdata } = useSWR(auth?.securityLevel === "3" ? underOppfolgingUrl : null, fetcher);
+  const underOppfolgingNivaa3 = underOppfolgingdata?.erBrukerUnderOppfolging;
+
+  const { data: oppfolgingsdata } = useSWR<Oppfolgingdata>(auth?.securityLevel === "4" ? oppfolgingUrl : null, fetcher);
+  const underOppfolgingNivaa4 = oppfolgingsdata?.underOppfolging;
+
+  const { data: sykemeldtdata } = useSWR<Brukerdata>(brukerinfoUrl, fetcher);
+  const erSykmeldtMedArbeidsgiver = sykemeldtdata?.erSykmeldtMedArbeidsgiver;
+
   return (
     <div className="podlet-vta-okonomi">
-      <OkonomiRad />
+      <OkonomiRad
+        oppfolgingNivaa3={underOppfolgingNivaa3}
+        oppfolgingNivaa4={underOppfolgingNivaa4}
+        erSykmeldtMedArbeidsgiver={erSykmeldtMedArbeidsgiver}
+      />
     </div>
   );
 }
--- src/components/okonomi/okonomi-rad.tsx
+++ src/components/okonomi/okonomi-rad.tsx
@@ -5,43 +5,19 @@ import DagpengerBilde from "./dagpenger";
 import NyRettTilSykepengerBilde from "./ny-rett-til-sykepenger";
 import OkonomiskSosialhjelpBilde from "./okonomisk-sosialhjelp";
 import { dagpengerLesmerLenke, sosialhjelpLenke, sykepengerLenke } from "../../lenker";
-import useSWR from "swr";
 import OkonomiRadDagpenger from "./okonomi-rad-dagpenger";
-import { authUrl, brukerinfoUrl, oppfolgingUrl } from "../../url";
 
-interface Authdata {
-  authenticated: boolean;
-  securityLevel: "3" | "4";
-  name: string;
+export interface Okonomiprops {
+  oppfolgingNivaa4: boolean | undefined;
+  oppfolgingNivaa3: boolean | undefined;
+  erSykmeldtMedArbeidsgiver: boolean | undefined;
 }
 
-interface Oppfolgingdata {
-  underOppfolging: string;
-}
+const OkonomiRad = (props: Okonomiprops) => {
+  const { oppfolgingNivaa3, oppfolgingNivaa4, erSykmeldtMedArbeidsgiver } = props;
+  const kanViseKomponent = oppfolgingNivaa3 || oppfolgingNivaa4;
 
-interface Brukerdata {
-  erSykmeldtMedArbeidsgiver: boolean;
-}
-
-const fetcher = async (url: string) => {
-  const response = await fetch(url, { method: "GET", credentials: "include" });
-  const data = await response.json();
-  return data;
-};
-
-const OkonomiRad = () => {
-  const { data: auth } = useSWR<Authdata>(authUrl, fetcher);
-  const isLevel4 = auth?.securityLevel === "4";
-
-  const { data: oppfolgingsdata } = useSWR<Oppfolgingdata>(oppfolgingUrl, fetcher);
-  const underOppfolging = oppfolgingsdata?.underOppfolging;
-
-  const { data: sykemeldtdata } = useSWR<Brukerdata>(brukerinfoUrl, fetcher);
-  const erSykmeldtMedArbeidsgiver = sykemeldtdata?.erSykmeldtMedArbeidsgiver;
-
-  const kanViseKomponent = isLevel4 && underOppfolging;
-
-  return !kanViseKomponent ? null : !erSykmeldtMedArbeidsgiver ? (
+  return !kanViseKomponent ? null : !erSykmeldtMedArbeidsgiver || !oppfolgingNivaa4 ? (
     <OkonomiRadDagpenger />
   ) : (
     <div className="okonomi-rad blokk-l">
