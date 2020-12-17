12934a9dd4f41e8d0aadcd753e320ca922bffa7f
Michael Gundersen <21148282+theagilepadawan@users.noreply.github.com>
2020-12-17 12:57:58 +0100

    Legger til endepunkt for å hente oppfølgingsstatus for nivå 3

--- src/url.ts
+++ src/url.ts
@@ -6,21 +6,26 @@ function getEnvironment(): string {
   return environment;
 }
 
-//TODO: Bytte om URL-ene under slik at de er rett. Nå er dev-endepunktet satt til 'production' pga deploy til NAIS
-const OPPFOLGING_URL: any = {
-  development: "/veilarboppfolging/api/oppfolging",
-  production: "/person/layout-dittnav/podium-resource/podlet-vta-okonomi/api-oppfolging",
+const AUTH_URL: any = {
+  development: "https://api.nav.no/innloggingsstatus/auth",
+  production: "https://innloggingsstatus.dev.nav.no/person/innloggingsstatus/auth",
 };
 const BRUKERINFO_URL: any = {
   development: "/veilarbregistrering/api/startregistrering",
   production: "/person/layout-dittnav/podium-resource/podlet-vta-okonomi/api-veilarbregistrering",
 };
 
-const AUTH_URL: any = {
-  development: "https://api.nav.no/innloggingsstatus/auth",
-  production: "https://innloggingsstatus.dev.nav.no/person/innloggingsstatus/auth",
+const OPPFOLGING_URL: any = {
+  development: "/veilarboppfolging/api/oppfolging",
+  production: "/person/layout-dittnav/podium-resource/podlet-vta-okonomi/api-oppfolging",
+};
+
+const UNDER_OPPFOLGING_URL: any = {
+  development: "https://api.nav.no/dittnav-api/oppfolging",
+  production: "https://www.dev.nav.no/person/dittnav-api/oppfolging",
 };
 
 export const oppfolgingUrl = OPPFOLGING_URL[getEnvironment()];
+export const underOppfolgingUrl = UNDER_OPPFOLGING_URL[getEnvironment()];
 export const brukerinfoUrl = BRUKERINFO_URL[getEnvironment()];
 export const authUrl = AUTH_URL[getEnvironment()];
